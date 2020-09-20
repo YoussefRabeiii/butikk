3a7afcafb64b45593b5fd2a245cf86becdf1a278
Youssef Rabei <youssefrabei2003@gmail.com>
2020-09-20 18:44:55 +0200

    🚧 Change the header in axios.js to my Site URL

--- src/axios.js
+++ src/axios.js
@@ -5,11 +5,11 @@ const instance = axios.create({
 
   // baseURL: "http://localhost:5001/butikkken/us-central1/api", // Local Server (Emulator)
 
-  // headers: {
-  //   //   // "Content-Type": "application/json",
-  //   "Access-Control-Allow-Origin": "*",
-  //   //   "Access-Control-Allow-Origin": "https://us-central1-butikkken.cloudfunctions.net",
-  // },
+  headers: {
+    "Access-Control-Allow-Origin": "https://butikk.netlify.app",
+    // "Content-Type": "application/json",
+    //   "Access-Control-Allow-Origin": "https://us-central1-butikkken.cloudfunctions.net",
+  },
 });
 
 export default instance;
