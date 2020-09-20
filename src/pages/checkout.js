7619f8e2a62a485573d704ffb0d000b1542284af
Youssef Rabei <youssefrabei2003@gmail.com>
2020-09-20 19:16:37 +0200

    🚧 Remove the Headers from the post request

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -36,14 +36,7 @@ const Checkout = () => {
       // Stripe need the total to be in currencies subUnits (like cents in dollars)
 
       const response = await axios.post(
-        `/checkout/create?total=${getBasketTotal(basket) * 100}`,
-        { body: "Post Body" },
-        {
-          headers: {
-            "Test-Header": "test-value",
-            "Access-Control-Allow-Origin": "https://butikk.netlify.app",
-          },
-        }
+        `/checkout/create?total=${getBasketTotal(basket) * 100}`
       );
 
       setClientSecret(response.data.clientSecret);
