23c679ac5912861202dc128f5364a0b1b82ca6a1
Youssef Rabei <youssefrabei2003@gmail.com>
2020-09-20 19:07:16 +0200

    🚧 Revert the post request

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -35,21 +35,16 @@ const Checkout = () => {
     const getClientSecret = async () => {
       // Stripe need the total to be in currencies subUnits (like cents in dollars)
 
-      // const response = await axios.post(
-      //   `/checkout/create?total=${getBasketTotal(basket) * 100}`,
-      //   { body: "Post Body" },
-      //   {
-      //     headers: {
-      //       "Test-Header": "test-value",
-      //       "Access-Control-Allow-Origin": "https://butikk.netlify.app",
-      //     },
-      //   }
-      // );
-
-      const response = await axios({
-        method: "post",
-        url: `/checkout/create?total=${getBasketTotal(basket) * 100}`,
-      });
+      const response = await axios.post(
+        `/checkout/create?total=${getBasketTotal(basket) * 100}`,
+        { body: "Post Body" },
+        {
+          headers: {
+            "Test-Header": "test-value",
+            "Access-Control-Allow-Origin": "https://butikk.netlify.app",
+          },
+        }
+      );
 
       setClientSecret(response.data.clientSecret);
     };
